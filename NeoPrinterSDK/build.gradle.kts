22114f7f2babff49492ee1d36af4811cee1a96a1
NandoDev1997 <luisfernandomoralescortes1997@gmail.com>
2026-03-31 12:05:59 -0600

    fix dependencies resolution with resources adding material

--- NeoPrinterSDK/build.gradle.kts
+++ NeoPrinterSDK/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.activity:activity-ktx:1.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("com.google.android.material:material:1.11.0")
 }
 
 publishing {
